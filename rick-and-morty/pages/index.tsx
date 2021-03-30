63a4d83cc6a6d2fe32aef4e4b4ebf4d2ebac655e
Mark Dunbavan <mdunbavan@gmail.com>
2021-03-30 11:14:59 +0100

    tweak the intro component, I can still use it

--- rick-and-morty/pages/index.tsx
+++ rick-and-morty/pages/index.tsx
@@ -1,8 +1,8 @@
-import { NextPage } from 'next';
-import IntroTemplate from '../src/components/_templates/Intro';
+import { NextPage } from "next";
+import IntroTemplate from "../src/components/_templates/Intro";
 
 const IntroPage: NextPage = () => {
-  return <IntroTemplate title="Rick and Morty FE Tech Test" />;
+  return <IntroTemplate title="Mark's Rick and Morty test" />;
 };
 
 export default IntroPage;
