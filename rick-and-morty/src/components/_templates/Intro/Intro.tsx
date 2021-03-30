092546065df9214731689f3e784494743d4fd74c
Mark Dunbavan <mdunbavan@gmail.com>
2021-03-30 11:16:35 +0100

    my own take :)

--- rick-and-morty/src/components/_templates/Intro/Intro.tsx
+++ rick-and-morty/src/components/_templates/Intro/Intro.tsx
@@ -1,32 +1,22 @@
-import React from 'react';
-import styled from 'styled-components';
-import Meta from '../../_shared/Meta';
+import React from "react";
+import styled from "styled-components";
+import Meta from "../../_shared/Meta";
 
 interface IIntroTemplateProps {
   className?: string;
   title: string;
 }
 
-const IntroTemplate: React.FC<IIntroTemplateProps> = ({
-  className,
-  title
-}) => {
+const IntroTemplate: React.FC<IIntroTemplateProps> = ({ className, title }) => {
   return (
     <>
       <Meta title={title} />
       <Wrapper className={className}>
         <IntroImage src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
 
-        <IntroCopy>
-          Please use the <strong>README.md</strong> file located in this repo to start the challenge.
-        </IntroCopy>
-        <IntroCopy>
-          Remember you do not need to complete both parts of this task. See how far you can get.
-        </IntroCopy>
-        <IntroCopy>
-          If you cannot complete it, then please ensure you can let us know what it is you would have done should you have had more time.
-        </IntroCopy>
-        <IntroCopy>** You can delete this file 👍</IntroCopy>
+        <h1>{title}</h1>
+
+        <CTA href="/characters">See all the Rick's</CTA>
       </Wrapper>
     </>
   );
@@ -50,7 +40,20 @@ const IntroImage = styled.img`
 const IntroCopy = styled.p`
   font-size: 18px;
   line-height: 1.5;
+  color: black;
+`;
+
+const CTA = styled.a`
+  font-size: 16px;
+  line-height: 1.5;
+  text-align: center;
+  padding: 10px 14px;
+  background-color: #97d7d7;
   color: white;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.8;
+  }
 `;
 
 export default IntroTemplate;
