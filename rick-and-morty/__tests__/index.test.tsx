f03cba9787788ca7972bb29a3cacda8c2fdae569
Mark Dunbavan <mdunbavan@gmail.com>
2021-03-30 11:27:00 +0100

    an example test has been added, making it pass :(

---
+++ rick-and-morty/__tests__/index.test.tsx
@@ -0,0 +1,16 @@
+import { mount, shallow } from "enzyme";
+import React from "react";
+
+import App from "../pages/characters/index";
+
+describe("Loading Alive rick's", () => {
+  it("App shows a title", () => {
+    const app = shallow(<App />);
+    expect(app.find("h1").text()).toEqual("Rick & Morty");
+  });
+
+  it("shows a list of characters", () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find("ul").children()).toBeGreaterThanOrEqual(1);
+  });
+});
