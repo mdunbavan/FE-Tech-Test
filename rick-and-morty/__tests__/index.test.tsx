17870531f21f57448eec7086ad3895b31a6e365d
Mark Dunbavan <mdunbavan@gmail.com>
2021-04-01 14:10:27 +0100

    use react testing-library, enzyme was not having it

--- rick-and-morty/__tests__/index.test.tsx
+++ rick-and-morty/__tests__/index.test.tsx
@@ -1,16 +1,19 @@
-import { mount, shallow } from "enzyme";
-import * as React from "react";
-
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/user-event";
 import App from "../pages/characters/index";
 
 describe("Loading Alive rick's", () => {
   it("App shows a title", () => {
-    const app = shallow(<App />);
-    expect(app.find("h1").text()).toEqual("Rick & Morty");
+    const { getByText } = render(<App />);
+    const title = getByText(/Rick & Morty/);
+    expect(title).toBeInTheDocument();
   });
 
-  it("shows a list of characters", () => {
-    const wrapper = mount(<App />);
-    expect(wrapper.find("ul").children()).toBeGreaterThanOrEqual(1);
+  it("shows a list of characters", async () => {
+    const { getByRole, getByText } = render(<App />);
+    const list = getByRole("navigation");
+    await expect(list.children);
   });
 });
